d57cb79bbe6793a5ef438729348affd854292587
Sean Tiner <156948641+SMT02@users.noreply.github.com>
2024-10-26 13:38:36 -0400

    Uploaded data files for the products

---
+++ elysian-frontend/src/components/sofasData.js
@@ -0,0 +1,131 @@
+// src/components/sofasData.js
+const sofasData = [
+  {
+    id: 1,
+    name: 'Nihon Sofa',
+    price: 6000.00,
+    image: '/img/sofaImg/productSofa1.png',
+    make: 'Zeitaku'
+  },
+  {
+    id: 2,
+    name: 'Shiki Sofa',
+    price: 7950.00,
+    image: '/img/sofaImg/productSofa2.png',
+    make: 'Zeitaku'
+  },
+  {
+    id: 3,
+    name: 'Kaze Sofa',
+    price: 5850.00,
+    image: '/img/sofaImg/productSofa3.png',
+    make: 'Zeitaku'
+  },
+  {
+    id: 4,
+    name: 'Luxus Sofa',
+    price: 5450.00,
+    image: '/img/sofaImg/productSofa4.png',
+    make: 'Mannheim'
+  },
+  {
+    id: 5,
+    name: 'Stilvoll Sofa',
+    price: 6650.00,
+    image: '/img/sofaImg/productSofa5.png',
+    make: 'Mannheim'
+  },
+  {
+    id: 6,
+    name: 'Kunst Sofa',
+    price: 7100.00,
+    image: '/img/sofaImg/productSofa6.png',
+    make: 'Mannheim'
+  },
+  {
+    id: 7,
+    name: 'Lys Sofa',
+    price: 5850.00,
+    image: '/img/sofaImg/productSofa7.png',
+    make: 'Gjøvik'
+  },
+  {
+    id: 8,
+    name: 'Bergstol Sofa',
+    price: 7800.00,
+    image: '/img/sofaImg/productSofa8.png',
+    make: 'Gjøvik'
+  },
+  {
+    id: 9,
+    name: 'Harmoni Sofa',
+    price: 5500.00,
+    image: '/img/sofaImg/productSofa9.png',
+    make: 'Gjøvik'
+  },
+  {
+    id: 10,
+    name: 'Canyon Sectional',
+    price: 16000.00,
+    image: '/img/sofaImg/productSofa10.png',
+    make: 'Jasper'
+  },
+  {
+    id: 11,
+    name: 'Timberline Sofa',
+    price: 8500.00,
+    image: '/img/sofaImg/productSofa11.png',
+    make: 'Jasper'
+  },
+  {
+    id: 12,
+    name: 'Venture Sectional',
+    price: 17050.00,
+    image: '/img/sofaImg/productSofa12.png',
+    make: 'Jasper'
+  },
+  {
+    id: 13,
+    name: 'Ritmo Sofa',
+    price: 5650.00,
+    image: '/img/sofaImg/productSofa13.png',
+    make: 'Espada'
+  },
+  {
+    id: 14,
+    name: 'Encanto Sofa',
+    price: 6450.00,
+    image: '/img/sofaImg/productSofa14.png',
+    make: 'Espada'
+  },
+  {
+    id: 15,
+    name: 'Camino Sofa',
+    price: 8150.00,
+    image: '/img/sofaImg/productSofa15.png',
+    make: 'Espada'
+  },
+  {
+    id: 16,
+    name: 'Regal Sofa',
+    price: 6350.00,
+    image: '/img/sofaImg/productSofa16.png',
+    make: 'Kingsley'
+  },
+  {
+    id: 17,
+    name: 'Oxford Sofa',
+    price: 5250.00,
+    image: '/img/sofaImg/productSofa17.png',
+    make: 'Kingsley'
+  },
+  {
+    id: 18,
+    name: 'Canterbury Sofa',
+    price: 6000.00,
+    image: '/img/sofaImg/productSofa18.png',
+    make: 'Kingsley'
+  },
+];
+
+export default sofasData;
---
+++ elysian-frontend/src/components/tablesData.js
@@ -0,0 +1,131 @@
+// src/components/tablesData.js
+const tablesData = [
+  {
+    id: 1,
+    name: 'Tsuki Coffee Table',
+    price: 2950.00,
+    image: '/img/tableImg/productTable1.png',
+    make: 'Zeitaku'
+  },
+  {
+    id: 2,
+    name: 'Shibui Dining Table',
+    price: 5500.00,
+    image: '/img/tableImg/productTable2.png',
+    make: 'Zeitaku'
+  },
+  {
+    id: 3,
+    name: 'Mizuki Dining Table',
+    price: 5850.00,
+    image: '/img/tableImg/productTable3.png',
+    make: 'Zeitaku'
+  },
+  {
+    id: 4,
+    name: 'Eiche Dining Table',
+    price: 6700.00,
+    image: '/img/tableImg/productTable4.png',
+    make: 'Mannheim'
+  },
+  {
+    id: 5,
+    name: 'Klassik Dining Table',
+    price: 4550.00,
+    image: '/img/tableImg/productTable5.png',
+    make: 'Mannheim'
+  },
+  {
+    id: 6,
+    name: 'Nordlicht Dining Table',
+    price: 8400.00,
+    image: '/img/tableImg/productTable6.png',
+    make: 'Mannheim'
+  },
+  {
+    id: 7,
+    name: 'Lofoten Dining Table',
+    price: 4250.00,
+    image: '/img/tableImg/productTable7.png',
+    make: 'Gjøvik'
+  },
+  {
+    id: 8,
+    name: 'Skogen Coffee Table',
+    price: 3850.00,
+    image: '/img/tableImg/productTable8.png',
+    make: 'Gjøvik'
+  },
+  {
+    id: 9,
+    name: 'Lysheim Dining Table',
+    price: 6250.00,
+    image: '/img/tableImg/productTable9.png',
+    make: 'Gjøvik'
+  },
+  {
+    id: 10,
+    name: 'Pioneer Dining Table',
+    price: 11350.00,
+    image: '/img/tableImg/productTable10.png',
+    make: 'Jasper'
+  },
+  {
+    id: 11,
+    name: 'Aspen Dining Table',
+    price: 4400.00,
+    image: '/img/tableImg/productTable11.png',
+    make: 'Jasper'
+  },
+  {
+    id: 12,
+    name: 'Prairie Dining Table',
+    price: 7350.00,
+    image: '/img/tableImg/productTable12.png',
+    make: 'Jasper'
+  },
+  {
+    id: 13,
+    name: 'Tierra Coffee Table',
+    price: 3150.00,
+    image: '/img/tableImg/productTable13.png',
+    make: 'Espada'
+  },
+  {
+    id: 14,
+    name: 'Zahara Dining Table',
+    price: 5200.00,
+    image: '/img/tableImg/productTable14.png',
+    make: 'Espada'
+  },
+  {
+    id: 15,
+    name: 'Sangría Dining Table',
+    price: 4900.00,
+    image: '/img/tableImg/productTable15.png',
+    make: 'Espada'
+  },
+  {
+    id: 16,
+    name: 'Cotswold Dining Table',
+    price: 4100.00,
+    image: '/img/tableImg/productTable16.png',
+    make: 'Kingsley'
+  },
+  {
+    id: 17,
+    name: 'Regency Dining Table',
+    price: 5050.00,
+    image: '/img/tableImg/productTable17.png',
+    make: 'Kingsley'
+  },
+  {
+    id: 18,
+    name: 'Victoria Dining Table',
+    price: 4950.00,
+    image: '/img/tableImg/productTable18.png',
+    make: 'Kingsley'
+  },
+];
+
+export default tablesData;
